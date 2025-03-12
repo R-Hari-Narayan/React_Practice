6fa9fbaa4f8ca352b2305ab8955307a681e9a47d
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-03-12 19:52:11 +0530

    Some changes in passwordGenerator project

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = () => {}
 
   return (
     <>
+      <h1 className='text-4xl text-center text-white'>
+        Password Generator
+      </h1>
     </>
   )
 }
